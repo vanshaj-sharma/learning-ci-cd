eec2c023658e56b761389bb68e67ec944716472a
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-06-13 12:43:52 +0530

    thisissparta2

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -5,7 +5,7 @@ import { authOptions } from "./lib/auth";
 export default async function Page() {
   //testing to work with pr
   //is it working??
-  //this is sparta
+  //this is sparta not sarav
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect("/dashboard");
